58a2aae901473054b9066877879040a2458c7352
nenkam henry <154386887+Sauvergarde@users.noreply.github.com>
2024-11-14 08:44:03 +0100

    Script.sql

--- Script.sql
+++ Script.sql
@@ -7,8 +7,19 @@ prenomEtu char(20),
 dareNaissance date,
 adresseEtu char(15),
 villeEtu char(20),
-niveauEtu char(10),
+niveauEtu char(10)
 );
 CREATE TABLE specialite (
-
+codeSpec integer primary key,
+  nomSpec char(20),
+  descSpec char(20)
+);
+CREATE TABLE Formation(
+  codeForm integer primary key,
+  titreForm var(50),
+  dureForm time,
+  prixForm float,
+  codeSpec integer,
+  FOREIGN KEY (codeSpec) REFERENCES specialite(codeSpec)
 )
+
