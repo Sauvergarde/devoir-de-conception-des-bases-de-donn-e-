d61ee3a089e1f74aa1a23cc74a926d3297c6a408
nenkam henry <154386887+Sauvergarde@users.noreply.github.com>
2024-11-14 08:26:04 +0100

    Create Script.sql

---
+++ Script.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE centre_formation
+use centre_formation
+CREATE TABLE Etudiant(
+NumCNIEtu integer primarykey,
+NomEtu char(20),
+prenomEtu char(20),
+dareNaissance date,
+adresseEtu char(15),
+villeEtu char(20),
+niveauEtu char(10),
+);
+CREATE TABLE specialite (
+
+)
